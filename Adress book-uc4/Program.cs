ff223c877308aebe50b022d24a4b8cf776eeef52
“Varun13016” <dmevarun@gmail.com>
2021-11-15 10:33:10 +0530

    uc4

---
+++ Adress book-uc4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Adress_book_uc4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemovePerson person = new RemovePerson();
+            person.AddPerson();
+        }
+    }
+}
