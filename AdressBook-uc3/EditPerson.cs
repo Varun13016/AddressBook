f564f67ef2988e3212cd704d21f6307014dae927
“Varun13016” <dmevarun@gmail.com>
2021-11-15 10:29:04 +0530

    uc3

---
+++ AdressBook-uc3/EditPerson.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdressBook_uc3
+{
+    class EditPerson
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public int ZipCode { get; set; }
+        public Double PhoneNumber { get; set; }
+        public String Email { get; set; }
+
+        public void AddPerson()
+        {
+            EditPerson person = new EditPerson();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Address : ");
+            Address = Console.ReadLine();
+
+            Console.Write("Enter city: ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter the state: ");
+            person.State = Console.ReadLine();
+
+            Console.Write("zipCode: ");
+            person.ZipCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Email: ");
+            person.Email = Console.ReadLine();
+
+            List<EditPerson> People = new List<EditPerson>();
+            People.Add(person);
+
+
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("Address 1: " + person.Address);
+            Console.WriteLine("city: " + person.City);
+            Console.WriteLine("city: " + person.State);
+            Console.WriteLine("zipCode: " + person.ZipCode);
+            Console.WriteLine("Email: " + person.Email);
+
+
+        }
+
+    }
+
+}
---
+++ AdressBook-uc3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdressBook_uc3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EditPerson person = new EditPerson();
+            person.AddPerson();
+        }
+    }
+}
